60c49d502e71f66d52698653133b438af7226aff
James Lentzsch <jrlentzsch@gmail.com>
2021-09-14 14:57:30 -0700

    elements assigned variables in js file

---
+++ index.js
@@ -0,0 +1,11 @@
+window.addEventListener("DOMContentLoaded", event => {
+    let appNavButton = document.getElementById("app-nav-button");
+    let blogNavBUtton = document.getElementById("blog-nav-button");
+    let shopNavBUtton = document.getElementById("shop-nav-button");
+    let noteNavBUtton = document.getElementById("note-nav-button");
+    let gameNavBUtton = document.getElementById("game-nav-button");
+
+    let leftArrow = document.getElementById("left-arrow");
+    let rightArrow = document.getElementById("right-arrow");
+    
+})
