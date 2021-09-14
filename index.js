bdb50c33c0b07c040ee44aacacc3aa57d091fe17
James Lentzsch <jrlentzsch@gmail.com>
2021-09-14 15:11:46 -0700

    carousel logic started not working

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 window.addEventListener("DOMContentLoaded", event => {
-    let appNavButton = document.getElementById("app-nav-button");
-    let blogNavBUtton = document.getElementById("blog-nav-button");
-    let shopNavBUtton = document.getElementById("shop-nav-button");
-    let noteNavBUtton = document.getElementById("note-nav-button");
-    let gameNavBUtton = document.getElementById("game-nav-button");
+    const appNavButton = document.getElementById("app-nav-button");
+    const blogNavBUtton = document.getElementById("blog-nav-button");
+    const shopNavBUtton = document.getElementById("shop-nav-button");
+    const noteNavBUtton = document.getElementById("note-nav-button");
+    const gameNavBUtton = document.getElementById("game-nav-button");
 
-    let leftArrow = document.getElementById("left-arrow");
-    let rightArrow = document.getElementById("right-arrow");
-    
+    const leftArrow = document.getElementById("left-arrow");
+    const rightArrow = document.getElementById("right-arrow");
+
+    const appType = document.getElementById("content-text-apptype")
+
+    const pages = ["app", "blog", "shop", "note", "game", "grave"];
+    let currentPage = pages[0];
+
+    rightArrow.addEventListener("click", event => {
+        if (currentPage < pages.length) {
+            curentPage++
+            appType.innerText = currentPage
+        }
+    })
+
+    leftArrow.addEventListener("click", event => {
+        if (currentPage > 0) {
+            curentPage--
+            appType.innerText = currentPage
+        }
+    })
+
 })
